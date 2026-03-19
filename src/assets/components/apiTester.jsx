c92d639f78129b46d8735bb055fc85dd382e5b1c
unknown <Clark San Pedro>
2026-03-19 20:53:12 +0800

    added react router dom must install for other forks

---
+++ src/assets/components/apiTester.jsx
@@ -0,0 +1,13 @@
+// the main ui of the api tester, will be used to test the api and display the results
+
+import { useState } from 'react'
+
+function apiTester() {
+  return (
+    <>
+      <div>
+        <h1>Brainstorming for the main frontend.</h1>
+      </div>
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+//Use this jsx file for the actual for the overall addition of dependencies within the app
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
